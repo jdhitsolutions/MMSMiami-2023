41299b5e78a5263b81ed06b1639796be2f165bd8
Jeff Hicks <jhicks@jdhitsolutions.com>
2023-10-05 15:37:53 -0400

    managing at scale demos

--- ManagingAtScale/demos/demo1.ps1
+++ ManagingAtScale/demos/demo1.ps1
@@ -112,37 +112,268 @@ Select-Object -Property * -ExcludeProperty RunspaceID
 #a variation
 Invoke-Command -ScriptBlock {
     $log = Get-CimInstance win32_NTEventLogFile -Filter "LogFileName='Security'"
-    if (($log.FileSize / $log.MaxFileSize)*100 -ge 80) {
+    if (($log.FileSize / $log.MaxFileSize) * 100 -ge 80) {
         $log | Select-Object @{Name = 'ComputerName'; Expression = { $_.CSName } },
         LogFileName, FileSize, MaxFileSize, NumberOfRecords,
         @{Name = 'PctUsed'; Expression = { ($_.FileSize / $_.MaxFileSize) * 100 } }
     }
-} -session $pssess -HideComputerName |
+} -Session $pssess -HideComputerName |
 Select-Object -Property * -ExcludeProperty RunspaceID
 
 Remove-PSSession $pssess
 #endregion
 #region Scaling with jobs
 
+
+#sequential
+Function Get-FolderReport {
+    [cmdletbinding()]
+    [OutputType('folderReport')]
+    [alias('gfp')]
+    Param(
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline,
+            HelpMessage = 'Specify the top-level folder'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ Test-Path $_ })]
+        [string]$Path = '.'
+    )
+
+    Begin {
+        Write-Verbose "[$((Get-Date).TimeOfDay) BEGIN  ] Starting $($MyInvocation.MyCommand)"
+        Write-Verbose "[$((Get-Date).TimeOfDay) BEGIN  ] Running under PowerShell version $($PSVersionTable.PSVersion)"
+
+    } #begin
+
+    Process {
+        $cPath = Convert-Path $Path
+        Write-Verbose "[$((Get-Date).TimeOfDay) PROCESS] Processing $cPath "
+        #ignore errors like Access Denied
+        $stats = Get-ChildItem -Path $cPath -File -Recurse -ErrorAction SilentlyContinue |
+        Measure-Object -Property Length -Sum -Average
+        [PSCustomObject]@{
+            PSTypeName  = 'folderReport'
+            Path        = $cPath
+            FileCount   = $stats.Count
+            TotalSize   = $stats.Sum
+            AverageSize = $stats.Average
+        }
+    } #process
+
+    End {
+        Write-Verbose "[$((Get-Date).TimeOfDay) END    ] Ending $($MyInvocation.MyCommand)"
+    } #end
+
+} #close Get-FolderReport
+
+<#
+"c:\work","c:\windows","c:\scripts",$home | Get-FolderReport
+50 seconds
+
+#>
 #background jobs
+
+Function Get-FolderReport {
+    [cmdletbinding()]
+    [OutputType('folderReport')]
+    [alias('gfp')]
+    Param(
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline,
+            HelpMessage = 'Specify the top-level folder'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ Test-Path $_ })]
+        [string]$Path = '.'
+    )
+
+    Begin {
+        Write-Verbose "[$((Get-Date).TimeOfDay) BEGIN  ] Starting $($MyInvocation.MyCommand)"
+        Write-Verbose "[$((Get-Date).TimeOfDay) BEGIN  ] Running under PowerShell version $($PSVersionTable.PSVersion)"
+        $jobs = @()
+    } #begin
+
+    Process {
+        $cPath = Convert-Path $Path
+        Write-Verbose "[$((Get-Date).TimeOfDay) PROCESS] Starting background job for $cPath"
+        #ignore errors like Access Denied
+        -ArgumentList $cPath
+    } #process
+
+    End {
+        Write-Verbose "[$((Get-Date).TimeOfDay) END    ] Waiting for $($jobs.count) jobs to complete"
+        $Jobs | Wait-Job | Receive-Job -Keep
+        Write-Verbose "[$((Get-Date).TimeOfDay) END    ] Ending $($MyInvocation.MyCommand)"
+    } #end
+
+} #close Get-FolderReport
+
+#20 seconds
+# Data can remain in background jobs if you want it
 
 #thread jobs
 #Install-Module ThreadJobs
+#use the current thread
+#pass output to the host
+#inherits current location
+#can throttle
+Function Get-FolderReport {
+    [cmdletbinding()]
+    [OutputType('folderReport')]
+    [alias('gfp')]
+    Param(
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline,
+            HelpMessage = 'Specify the top-level folder'
+        )]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({ Test-Path $_ })]
+        [string]$Path = '.'
+    )
 
+    Begin {
+        Write-Verbose "[$((Get-Date).TimeOfDay) BEGIN  ] Starting $($MyInvocation.MyCommand)"
+        Write-Verbose "[$((Get-Date).TimeOfDay) BEGIN  ] Running under PowerShell version $($PSVersionTable.PSVersion)"
+        $jobs = @()
+    } #begin
+
+    Process {
+        $cPath = Convert-Path $Path
+        Write-Verbose "[$((Get-Date).TimeOfDay) PROCESS] Starting thread job for $cPath"
+        #ignore errors like Access Denied
+        -ArgumentList $cPath -StreamingHost $Host
+    } #process
+
+    End {
+        Write-Verbose "[$((Get-Date).TimeOfDay) END    ] Waiting for $($jobs.count) jobs to complete"
+        $Jobs | Wait-Job | Receive-Job -Keep
+        Write-Verbose "[$((Get-Date).TimeOfDay) END    ] Ending $($MyInvocation.MyCommand)"
+    } #end
+
+} #close Get-FolderReport
+
+# "c:\work","c:\windows","c:\scripts",$home | Get-FolderReport -Verbose
+#21 seconds
 #endregion
 
 #region ForEach -parallel
 
 #Sequential
+Measure-Command {
+    $r = 'c:\work', 'c:\windows', 'C:\temp', $home |
+    ForEach-Object {
+        $p = Convert-Path $_
+        $start = Get-Date
+        Write-Host "[$((Get-Date).TimeOfDay)] Analyzing $p" -ForegroundColor green
+        Get-ChildItem -Path $p -File -Recurse -ErrorAction SilentlyContinue |
+        Measure-Object -Property Length -Sum -Average |
+        Select-Object @{Name = 'Path'; Expression = { $p } }, Count, Sum, Average
 
+        Write-Host "[$((Get-Date).TimeOfDay)] Ending $p -> $(New-TimeSpan -Start $start -End (Get-Date))" -ForegroundColor yellow
+    }
+}
+#27ms
+
+#parallel - consider the overhead
+Measure-Command {
+    $r = 'c:\work', 'c:\windows', 'C:\temp', $home |
+    ForEach-Object -Parallel {
+        $p = Convert-Path $_
+        $start = Get-Date
+        Write-Host "[$((Get-Date).TimeOfDay)] Analyzing $p" -ForegroundColor green
+
+        Get-ChildItem -Path $p -File -Recurse -ErrorAction SilentlyContinue |
+        Measure-Object -Property Length -Sum -Average |
+        Select-Object @{Name = 'Path'; Expression = { $p } }, Count, Sum, Average
+        Write-Host "[$((Get-Date).TimeOfDay)] Ending $p -> $(New-TimeSpan -Start $start -End (Get-Date))" -ForegroundColor yellow
+    }
+}
+#21 ms
 
 #endregion
-#region PipelineVariable vs OutVariable
+#region PipelineVariable vs  OutVariable vs Tee-Object
+
+#outvariable
+Get-WinEvent -FilterHashtable @{LogName = 'System'; Level = 2 } -MaxEvents 1000 -OutVariable errLog |
+Group-Object -Property ProviderName -OutVariable go
+
+$errLog[0..10]
+$go[-1].Group
+
+#Tee-Object
+Get-WinEvent -FilterHashtable @{LogName = 'System'; Level = 2 } -MaxEvents 1000 -OutVariable errLog |
+Group-Object -Property ProviderName -OutVariable go | Tee-Object -Variable r
+
+$r
+
+#pipelinevariable
+
+Get-ChildItem c:\work -File -PipelineVariable source | Copy-Item -Destination c:\temp -PassThru -PipelineVariable dest |
+ForEach-Object {
+    $msg = "[$(Get-Date)] Copied {0} from {1} to {2}" -f $source.Name, $source.DirectoryName, $dest.DirectoryName
+    Write-Host $msg -ForegroundColor Yellow
+}
+
+#putting it all together
+#this is not the only way or even the best way to do this
+Get-WinEvent -FilterHashtable @{LogName = 'System'; Level = 2, 3, 4 } -MaxEvents 1000 -OutVariable sysLog -PipelineVariable get |
+Group-Object -Property ProviderName -OutVariable go -PipelineVariable group | Tee-Object -Variable t |
+select @{Name = 'LogName'; Expression = { $get.logname } },
+Name,
+@{Name = 'TotalCount'; Expression = { $_.Count } },
+@{Name = 'TotalPct'; Expression = { ($_.Count / ($t | measure count -Sum).sum) * 100 } },
+@{Name = 'ErrCount'; Expression = { ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Error' }).Count } },
+@{Name = 'ErrPct'; Expression = { ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Error' }).Count / $group.Count * 100 } },
+@{Name = 'WarnCount'; Expression = { ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Warning' }).Count } },
+@{Name = 'WarnPct'; Expression = { ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Warning' }).Count / $group.Count * 100 } },
+@{Name = 'InfoCount'; Expression = { ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Information' }).Count } },
+@{Name = 'InfoPct'; Expression = { ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Information' }).Count / $group.Count * 100 } }
 
 
 #endregion
 #region Leveraging Remoting
 
+$computers = 'dom1', 'dom2', 'srv1', 'srv2'
+#This assumes there will be no errors connecting to the computer
+Measure-command {
+    $r = foreach ($c in $computers) {
+        Write-Host "[$((Get-Date).TimeOfDay)] Processing $c" -ForegroundColor Green
+        Get-WinEvent -FilterHashtable @{LogName = 'System'; Level = 3, 4 } -ComputerName $c |
+        Group-Object -Property ProviderName | ForEach-Object {
+            [PSCustomObject]@{
+                ComputerName = $c.ToUpper()
+                LogName      = $_.group[0].LogName
+                Source       = $_.Name
+                TotalCount   = $_.Count
+                ErrCount     = ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Error' }).Count
+                WarnCount    = ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Warning' }).Count
+            }
+        }
+        Write-Host "[$((Get-Date).TimeOfDay)] Finished $c" -ForegroundColor yellow
+    } #foreach
+} #41 seconds
+
+Measure-Command {
+    $q=  Invoke-Command -ScriptBlock {
+        Write-Host "[$((Get-Date).TimeOfDay)] Processing $env:Computername" -ForegroundColor green
+        Get-WinEvent -FilterHashtable @{LogName = 'System'; Level = 3, 4 } |
+        Group-Object -Property ProviderName | ForEach-Object {
+            [PSCustomObject]@{
+                ComputerName =$env:Computername
+                LogName      = $_.group[0].LogName
+                Source       = $_.Name
+                TotalCount   = $_.Count
+                ErrCount     = ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Error' }).Count
+                WarnCount    = ($_.Group | Where-Object { $_.LevelDisplayName -eq 'Warning' }).Count
+            }
+        }
+        Write-Host "[$((Get-Date).TimeOfDay)] Finished $env:Computername" -ForegroundColor yellow
+    } -ComputerName $computers -HideComputerName | Select-Object -Property * -ExcludeProperty RunspaceID
+} #9 seconds
 
 #endregion
 
